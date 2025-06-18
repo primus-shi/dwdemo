2eca058129e372a9864f71812b3eed1fb6866bbd
Myj <1797548562@qq.com>
2025-06-18 17:45:50 +0800

    登录页面实现

---
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.example.demo.mapper;
+
+import com.example.demo.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+
+    @Select("select * from user where username = #{username} and password = #{password}")
+    List<User> selectUserByNameAndPassword(User user);
+}
