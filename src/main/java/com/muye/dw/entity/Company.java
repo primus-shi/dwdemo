e8d2bff78236361f336e7d6998388257917c768f
Myj <1797548562@qq.com>
2025-06-18 15:42:55 +0800

    update

--- src/main/java/com/muye/dw/entity/Company.java
+++ src/main/java/com/muye/dw/entity/Company.java
@@ -4,5 +4,10 @@ import lombok.Data;
 
 @Data
 public class Company {
-
+    private Integer id;
+    private String name;
+    private String bak;
+    private String tel;
+    private String email;
+    private  String address;
 }
